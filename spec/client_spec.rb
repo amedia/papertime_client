9d9613b14897de4fc41bb436187128a295a3a1db
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2011-10-02 14:30:04 +0200

    Add spec

---
+++ spec/client_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe PapertimeClient do
+  it "rubs the lotion on its skin" do
+    defined?(PapertimeClient).should eq("constant")
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'bundler'
+Bundler.require
+
+require File.dirname(__FILE__)+'/../lib/papertime_client.rb'
