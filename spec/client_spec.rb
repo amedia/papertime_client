012a469dc8e3c17840d2980b3af40e179aafb105
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2011-10-02 15:51:20 +0200

    Udpated tests

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
+# SERVER_ADDRESS is defined in spec_helper.
+
 describe PapertimeClient do
-  it "rubs the lotion on its skin" do
-    defined?(PapertimeClient).should eq("constant")
+
+  before :each do
+    @client = PapertimeClient.new(SERVER_ADDRESS)
   end
+
+  it "has base_url" do
+    @client.base_url.should eq("#{SERVER_ADDRESS}/api/v1/")
+  end
+
+  it "finds a deadline date for amta.no" do
+    @client.get_deadline_date(
+      :domain => 'amta.no',
+      :publication_date => Date.today+9,
+      :production_days => 4).class.should eq Date
+  end
+
+  it "finds publication for 'www.amta.no'" do
+    @client.find_publication_by_domain('www.amta.no')["publication"]["title"].should eq "AKERSHUS AMSTIDENDE"
+  end
+
+  it "finds publication for Fjellgaten 3, Bergen" do
+    @client.find_publications_by_lat_lon(
+      60.39722717832889, 5.329188849684669
+    )["publications"][0]["publication"]["title"].should eq "BERGENSAVISEN"
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,5 @@ require 'bundler'
 Bundler.require
 
 require File.dirname(__FILE__)+'/../lib/papertime_client.rb'
+
+SERVER_ADDRESS="http://127.0.0.1:3001"
